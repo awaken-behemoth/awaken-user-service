6cca9762c45f51fbc7a4144b18fccf91354824fa
Behemoth11 <kodohdaniel@gmail.com>
2022-07-10 23:54:23 -0500

    first commit

---
+++ src/test/java/awaken/awakenauthservice/AwakenAuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package awaken.awakenauthservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwakenAuthServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
